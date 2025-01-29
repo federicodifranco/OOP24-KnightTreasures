1ccdf3d14141f6c68cc357a8260c38abd688f180
Fabio Gruppioni <fabio.gruppioni2@studio.unibo.it>
2025-01-29 15:32:57 +0100

    Added player's animations and sprites, created Knight's and home's images

--- src/main/java/it/unibo/knightreasures/view/impl/ApplicationPanel.java
+++ src/main/java/it/unibo/knightreasures/view/impl/ApplicationPanel.java
@@ -10,12 +10,15 @@ public class ApplicationPanel extends JPanel {
 
     private MouseInputs MouseInputs;
     private float xDelta = 100, yDelta = 100;
-    private BufferedImage img;
+    private BufferedImage img,subImg;
+    private BufferedImage[][] animation;
+    private int aniTick, aniIndex, aniSpeed = 15;
 
     public ApplicationPanel() {
         mouseInputs = new MouseInputs(this);
 
         importImg();
+        loadAnimation();
 
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
@@ -23,11 +26,31 @@ public class ApplicationPanel extends JPanel {
     }
 
     private void importImg() {
-        img = ResourceFuncUtilities.loadSources("menu");
+        img = ResourceFuncUtilities.loadSources("knight");
+    }
+
+    private void loadAnimation(){
+        animation = new BufferedImage[4][6];
+        for(int j = 0; j < animation.length; j++) {
+            for(int i = 0; i < animation[j].length; i++) {
+                animation[j][i] = img.getSubimage(i * 83, j * 125, 83, 125);
+            }
+        }
     }
 
     public void changeXDelta(int value) {
         this.xDelta += value;
+    }
+
+    private void updateAnimation() {
+        aniTick++;
+        if (aniTick >= aniSpeed) {
+            aniTick = 0;
+            aniIndex++;
+            if (aniIndex >= idleAni.legth) {
+                aniIndex = 0;
+            }
+        }
     }
 
     public void changeYDelta(int value) {
@@ -42,7 +65,8 @@ public class ApplicationPanel extends JPanel {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(img, 100, 100, null);
+        updateAnimation();
+        g.drawImage(animation[2][2], (int)xDelta, (int)yDelta, 83,125, null);
 
     }
 
