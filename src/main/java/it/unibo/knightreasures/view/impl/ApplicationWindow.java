8f4233721af2664f70d1f1548a1762b871f0c623
Carmine Varipapa <carmine.varipapa@studio.unibo.it>
2025-01-29 14:15:00 +0100

    Added Application Window

--- src/main/java/it/unibo/knightreasures/view/impl/ApplicationWindow.java
+++ src/main/java/it/unibo/knightreasures/view/impl/ApplicationWindow.java
@@ -1,3 +1,17 @@
 public class ApplicationWindow {
 
+    private JFrame frame;
+
+    public ApplicationWindow(ApplicationPanel applicationPanel) {
+
+        frame = new JFrame();
+        frame.setSize(1280, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(applicationPanel);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+    }
+
 }
